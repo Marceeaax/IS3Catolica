cf7c9cd30e9af35d1bd4f3b7e7f6bfd348621d90
cacereslujann <cacereslujan183@gmail.com>
2024-04-18 19:48:31 -0400

    Subo mi base de datos Closes #7

---
+++ db/data.js
@@ -0,0 +1,22 @@
+// Datos de los integrantes
+exports.integrantes = [
+    {nombre: "Lujan", imagen: "", Codigo: "01"},
+    {nombre: "Christian", imagen: "", Codigo: "02"},
+    {nombre: "Steven", imagen: "", Codigo: "03"},
+    {nombre: "Marcio", imagen: "", Codigo: "04"},
+];
+
+exports.media = [
+    {
+        nombre: "X / Tw",
+    },
+    {
+        nombre: "YouTube",
+    },
+    {
+        nombre: "Blog",
+    },
+];
+
+//exports.integrantes = integrantes;
+//exports.media2 = media;
