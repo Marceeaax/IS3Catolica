6191e89f5aaee91cb8ba568e140c4f4ddbee72b7
cacereslujann <cacereslujan183@gmail.com>
2024-04-24 16:03:35 -0400

    Cada pagina individual se transformo de manera dinamica Ref #10

--- db/data.js
+++ db/data.js
@@ -1,22 +1,42 @@
 // Datos de los integrantes
 exports.integrantes = [
-    {nombre: "Lujan", apellido: "Caceres", imagen: "", Codigo: "01", matricula: "Y18624"},
-    {nombre: "Christian", apellido: "Salinas", imagen: "", Codigo: "02", matricula: "Y25366"},
-    {nombre: "Steven", apellido: "Lopez", imagen: "", Codigo: "03", matricula: "Y12887"},,
-    {nombre: "Marcio", apellido: "Saldivar", imagen: "", Codigo: "04", matricula: "3850665"},
+    {nombre: "Lujan", apellido: "Caceres", imagen: "", Codigo: "01", matricula: "Y18624"}, //indice 0
+    {nombre: "Christian", apellido: "Salinas", imagen: "", Codigo: "02", matricula: "Y25366"}, //indice 1
+    {nombre: "Steven", apellido: "Lopez", imagen: "", Codigo: "03", matricula: "Y12887"}, //indice 2
+    {nombre: "Marcio", apellido: "Saldivar", imagen: "", Codigo: "04", matricula: "3850665"}, //indice 3
 ];
 
-exports.media = [
-    {
-        nombre: "X / Tw",
+exports.media = {
+    //Lujan
+    "Y18624": {
+        youtube: "https://www.youtube.com/embed/Ci5raxp37QE", 
+        imagen: "../../images/isla.jpeg", 
+        dibujo: "../../images/paint_lujan.png", 
+        matricula: "Y18624"
     },
-    {
-        nombre: "YouTube",
+    // Christian
+    "Y25366": {
+        youtube: "https://www.youtube.com/embed/ZDs_f_ZdluU", 
+        imagen: "../../images/imgrepresentativa.jpg", 
+        dibujo: "../../images/imgpropia.png", 
+        matricula: "Y25366"
     },
-    {
-        nombre: "Blog",
+    // Steven
+    "Y12887": {
+        youtube: "https://www.youtube.com/embed/0aQPX_Iqu-A?si=yrpnMk0IUVFkQtif", 
+        imagen: "../../images/mejores-anime-largos.webp", 
+        dibujo: "../../images/20240410_192335.jpg", 
+        matricula: "Y12887"
     },
-];
+    //Marcio
+    "3850665": {
+        youtube: "https://www.youtube.com/embed/QcaZLO59Vpc?si=qS5-wBEcKhgr_DaM", 
+        imagen: "../../images/mathi.jpg", 
+        dibujo: "../../images/imagen.png", 
+        matricula: "3850665"
+    }
+};
+
 
 //exports.integrantes = integrantes;
 //exports.media2 = media;
