c798e81ba94691e0b5f1719b209a4c17158ac719
cacereslujann <cacereslujan183@gmail.com>
2024-05-09 00:52:46 -0400

    Agregar las configuraciones y el archivo de conexion a la base de datos SQLite Ref #6

---
+++ db/database.js
@@ -0,0 +1,17 @@
+// database.js
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Define el camino de la base de datos dentro de tu proyecto
+const dbPath = path.join(__dirname, 'bd', 'mi_base_datos.sqlite');
+
+// Crea la conexión a la base de datos
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error('Error al conectar con la base de datos:', err.message);
+    } else {
+        console.log('Conexión a la base de datos establecida.');
+    }
+});
+
+module.exports = db;
