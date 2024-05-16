886513a031db011b05c2f4b034fe48c6df96e827
cacereslujann <cacereslujan183@gmail.com>
2024-05-15 23:44:56 -0400

    Agregar estilos a la pagina de admin. Ref #1

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("admin/index",{
-        mostrarAdmin: true
+        mostrarAdmin: true,
+        footerfijo: true
     });
 });
 
